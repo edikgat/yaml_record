ccf029929114e342d56adc22f328f5547355e1ed
Ruslan Hamidullin <ror_developer@mail.ru>
2013-08-21 12:18:18 +0400

    Added dynamic creation of folders and files

--- lib/yaml_record/adapters/local_store.rb
+++ lib/yaml_record/adapters/local_store.rb
@@ -11,6 +11,12 @@ module YamlRecord
       #   @adapter.read("foo") => [{...}, {...}]
       #
       def read(source, dynamic_source, source_file_name)
+        dir_path = source + dynamic_source
+        full_path = dir_path + source_file_name
+
+        Dir::mkdir(dir_path) unless File.directory?(dir_path)
+        File.open(full_path, 'w') unless File.exists?(full_path)
+
         YAML.load_file(source + dynamic_source + source_file_name)
       end
 
@@ -21,6 +27,12 @@ module YamlRecord
       #   @adapter.write("foo", [{...}, {...}]) => "<yaml data>"
       #
       def write(source, source_file_name, dynamic_source, collection)
+        dir_path = source + dynamic_source
+        full_path = dir_path + source_file_name
+
+        Dir::mkdir(dir_path) unless File.directory?(dir_path)
+        File.open(full_path, 'w') unless File.exists?(full_path)
+        
         File.open(source + dynamic_source + source_file_name, 'w') {|f| f.write(collection.to_yaml) }
       end
     end
